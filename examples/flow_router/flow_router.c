72f857d1eba7633df434a2602446a7ea5405d2b3
Hand-some <31262124+Hand-some@users.noreply.github.com>
2018-12-29 10:32:01 +0800

    Update flow_router.c

--- examples/flow_router/flow_router.c
+++ examples/flow_router/flow_router.c
@@ -204,7 +204,7 @@ get_rte_hash_table(void){
 }
 
 static int
-packet_handler(struct rte_mbuf *pkt, struct onvm_pkt_meta *meta, __attribute__((unused)) struct onvm_nf_info *nf_info) {
+packet_handler(struct rte_mbuf *pkt, struct onvm_pkt_meta *meta) {
     	static uint32_t counter = 0;
 	static int flag = 1, flag_hash_table = 1;
 	int flag_file_read;//flag_file_read is to signal if the file has the hash key
@@ -235,7 +235,7 @@ packet_handler(struct rte_mbuf *pkt, struct onvm_pkt_meta *meta, __attribute__((
         	printf("Error in flow lookup: %d (ENOENT=%d, EINVAL=%d)\n", tbl_index, ENOENT, EINVAL);
         	onvm_pkt_print(pkt);
         	#endif
-		onvm_nflib_stop(nf_info);
+		onvm_nflib_stop();
 		rte_exit(EXIT_FAILURE, "Error in flow lookup\n");
 	}
 	else {
